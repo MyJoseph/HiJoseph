4224c9e84fd2ea186e000877c064067e984046b8
MyJoseph <34302985+MyJoseph@users.noreply.github.com>
2017-12-07 10:13:42 +0800

    third

---
+++ AndAndTest.java
@@ -0,0 +1,23 @@
+package Josephlee;
+/**
+*@tuthor Josephlee
+*<br>Date:
+*<br>Program Name:
+*@version 1.0
+*/
+ 
+public class AndAndTest
+{
+	public static void main(String[] args)
+	{
+		int i = 1 ;
+		int j=1;
+		int y=1;
+		System.out.println(i++);  //测试i++
+		System.out.println(++i);  //测试++i
+		System.out.println(++j);
+	
+        System.out.println(~ -5);  //测试按位取反
+        System.out.println(y+=5);
+	}
+}
